98a3b5aae63d72ff7b57aaae2b6c30f2d0ce9a9a
LEESOOYE <sooye7@naver.com>
2022-04-30 20:47:55 +0900

    Feat : 상품 등록 기능(파일 업로드), 상품 등록 수정 기능 추가 Add WebMvcConfig.java, ItemService.java, ItemImgService.java, FileService.java ItemImgRepository.java Add ItemServiceTest.java for test Update Item.java, ItemController.java

--- src/main/java/com/shop/entity/Item.java
+++ src/main/java/com/shop/entity/Item.java
@@ -2,6 +2,7 @@ package com.shop.entity;
 
 
 import com.shop.constant.ItemSellStatus;
+import com.shop.dto.ItemFormDto;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -49,4 +50,11 @@ public class Item {
     )
     private List<Member> member;
 
+    public void updateItem(ItemFormDto itemFormDto){
+        this.itemNm=itemFormDto.getItemNm();
+        this.price=itemFormDto.getPrice();
+        this.stockNumber=itemFormDto.getStockNumber();
+        this.itemDetail=itemFormDto.getItemDetail();
+        this.itemSellStatus=itemFormDto.getItemSellStatus();
+    }
 }
