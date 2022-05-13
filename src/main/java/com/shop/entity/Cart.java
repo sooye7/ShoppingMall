fff910d11864fad4ea1a8b7962da47af15f6ed00
LEESOOYE <sooye7@naver.com>
2022-05-13 23:55:40 +0900

    Feat : 장바구니 기능 추가 Update Cart.java, CartItem.java, CartRepository.java Update CartItemDto.java, CartItemRepository.java, CartService.java

--- src/main/java/com/shop/entity/Cart.java
+++ src/main/java/com/shop/entity/Cart.java
@@ -21,4 +21,10 @@ public class Cart {
     @JoinColumn(name = "member_id") // JoinColumn 매핑할 외래키를 지정합니다. 외래키 이름을 설정
     //name을 명시하지않으면 JPA가 알아서 ID를 찾지만 원하는 이름이 아닐 수도 있습니다.
     private Member member;
+
+    public static Cart createCart(Member member){
+        Cart cart = new Cart();
+        cart.setMember(member);
+        return cart;
+    }
 }
